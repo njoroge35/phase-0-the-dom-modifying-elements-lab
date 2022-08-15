cce246e06a59d2a055b13519a11d64b4970557cc
David <david.njoroge@student.moringaschool.com>
2022-08-15 22:38:29 +0300

    dom modifying

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here!// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader= document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute("id",'victory');
+
+// const firstName= "Grace";
+newHeader.innerHTML="Grace is the champion";
